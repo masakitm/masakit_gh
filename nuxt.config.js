417d5acd1da9eaeee14392e0b0fa36cb30f3f57b
masaki <tomotsuka@thisco.jp>
2018-01-02 23:13:03 +0900

    masakit hanei

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   head: {
-    title: 'Masaki Tomtosuka',
+    title: 'Masaki Tomotsuka',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
